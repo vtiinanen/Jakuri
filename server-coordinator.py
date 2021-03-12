9de03ab51c4cb506861fa6d3962f54b8f39cba81
vili.tiinanen <vili.tiinanen@fidelix.fi>
2021-03-12 12:45:19 +0200

    Reduce the starting delay

--- server-coordinator.py
+++ server-coordinator.py
@@ -166,9 +166,9 @@ def main(arguments):
             firstFlag = False
             distributor.send_ping()
 
-        # 3s non-blocking delay
+        # 0.5s non-blocking delay
         now = int(time.time())
-        if now - timeStamp > 1:
+        if now - timeStamp > 0.5:
             startFlag = True
 
         if startFlag == True and startFlag != startFlagOld and d_args["print_amount"]:
