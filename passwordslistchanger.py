d41d5ac3ec918930c6e0abddf3b884d5e2f2be78
Jarkko Kotaniemi <jarkko.kotaniemi@vtt.fi>
2021-03-13 13:57:55 +0200

    fixed endline bug

--- passwordslistchanger.py
+++ passwordslistchanger.py
@@ -21,24 +21,34 @@ def main(arguments):
         help='Output argument file',
         required=True
     )
+    parser.add_argument(
+        '-a',
+        '--attempts',
+        help="Attempt amount",
+        default=10,
+        type=int
+    )
 
     args = parser.parse_args(arguments)
     input_lines = args.inputfile.readlines()
     args.inputfile.close()
-
-    f = open(args.outputfile, 'w')
-    i = 0
+    f = open(args.outputfile, 'w', encoding="UTF-8")
+    numofnewlines = 1
     newlinelist = []
-    for line in input_lines:
-        line.rstrip("\n")
-        newlinelist.append(line)
-        if i%10 == 0 and i!=0:
+    lastlineslist = []
+    for i, line in enumerate(input_lines):
+        newlinelist.append(line.rstrip("\n"))
+        if i%args.attempts == 0 and i!=0:
             newlinelist.insert(0, '87e93406a19d11166fd4aff9addf299aad2221cbd45febc596a527b65269b78f')
-            print(newlinelist)
             newline = ','.join(newlinelist)
             f.writelines(f'{newline}' + '\n')
             newlinelist = []
-        i = i+1
+            numofnewlines = numofnewlines+1
+        if i > len(input_lines)-args.attempts and i > args.attempts and i > len(input_lines)-numofnewlines*args.attempts:
+            lastlineslist.append(line.rstrip("\n"))
+    lastlineslist.insert(0, '87e93406a19d11166fd4aff9addf299aad2221cbd45febc596a527b65269b78f')
+    newline = ','.join(lastlineslist)
+    f.writelines(f'{newline}' + '\n')
     f.close()
             
 
