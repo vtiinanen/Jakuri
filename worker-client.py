204bf285c774e4a708e2ceea6c5bd3b9652382da
vili.tiinanen <vili.tiinanen@fidelix.fi>
2021-03-24 10:41:33 +0200

    Add threading

--- worker-client.py
+++ worker-client.py
@@ -4,24 +4,20 @@
 Worker client for Jakuri
 """
 
+import time
+from threading import Thread
+from hashlib import sha256
+
 import redis
 import shortuuid
 from more_itertools import nth_product
 
-import time
-from hashlib import sha256
 
 ID = f'{shortuuid.uuid()}'
-
-
-def Fibonacci(n):
-    assert n >= 0
-    if n == 0:
-        return 0
-    if n == 1 or n == 2:
-        return 1
-    return Fibonacci(n-1) + Fibonacci(n-2)
-
+STATE = 0
+CMDS = ['STATE', 'PING', 'KILL', 'SLEEP']
+FUNCTIONS = ['shacrackprod', 'shacrackprod']
+exit_sign = False
 
 def Sha256Crack(args):
     result = ""
@@ -52,64 +48,116 @@ def Sha256CrackProd(hash, start, end, chars, length):
     return result
 
 
-class Listener():
+class CmdListener(Thread):
 
     def __init__(self, r):
+        Thread.__init__(self)
+        self.redis = r
+        self.pubsub = self.redis.pubsub()
+        self.pubsub.subscribe(['lobby'])
+        self.pubsub.psubscribe([f'worker-{ID}.*'])
+
+    def run(self):
+        global ID, STATE
+        global exit_sign
+
+        for msg in self.pubsub.listen():
+            if not msg:
+                continue
+
+            if msg['type'] == 'subscribe':
+                continue
+
+            if msg['type'] == 'psubscribe':
+                continue
+
+            if 'STATE' in msg['data']:
+                self.redis.publish(f'worker-{ID}', f'STATE {STATE}')
+
+            if 'PING' in msg['data']:
+                self.redis.publish(f'worker-{ID}', "PONG")
+
+            if f'worker-{ID}.' not in msg['channel']:
+                continue
+                
+            msg['channel'] = msg['channel'].split(".")[1]
+
+            if msg['channel'] not in CMDS:
+                continue
+
+            if 'KILL' in msg['channel']:
+                STATE = 2
+                exit_sign = True
+
+            if 'SLEEP' in msg['channel']:
+                STATE = 2
+                id = msg['data'].split(' ')[0]
+                time.sleep(2)
+                self.redis.publish(f'worker-{ID}.result', f'{id} slept')
+
+            if exit_sign:
+                break
+
+            STATE = 1
+
+
+class FunctionListener(Thread):
+
+    def __init__(self, r):
+        Thread.__init__(self)
         self.redis = r
         self.pubsub = self.redis.pubsub()
         self.pubsub.psubscribe([f'worker-{ID}.*'])
-        self.pubsub.subscribe(['lobby'])
 
     def run(self):
-        global ID
+        global ID, STATE, FUNCTIONS
+        global exit_sign
 
-        msg = self.pubsub.get_message()
-        if not msg:
-            return
+        for msg in self.pubsub.listen():
+            if not msg:
+                continue
 
-        if msg['type'] == 'subscribe':
-            return
+            if msg['type'] == 'subscribe':
+                continue
 
-        if msg['type'] == 'psubscribe':
-            return
+            if msg['type'] == 'psubscribe':
+                continue
 
-        if f'worker-{ID}.' in msg['channel']:
+            if f'worker-{ID}.' not in msg['channel']:
+                continue
+
             msg['channel'] = msg['channel'].split(".")[1]
 
-        if 'lobby' in msg['channel'] and 'PING' in msg['data']:
-            self.redis.publish(f'worker-{ID}', "PONG")
+            if msg['channel'] not in FUNCTIONS:
+                continue
 
-        if 'fibonacci' in msg['channel']:
-            args = msg['data'].split(' ')
-            id = args.pop(0)
-            num = args.pop(0)
-            result = Fibonacci(int(num))
-            self.redis.publish(f'worker-{ID}.result', f'{id} {result}')
+            if 'shacrack' == msg['channel']:
+                STATE = 2
+                args = msg['data'].split(' ')
+                id = args.pop(0)
+                result = Sha256Crack(args)
+                self.redis.publish(f'worker-{ID}.result', f'{id} {result}')
 
-        if 'sleep' in msg['channel']:
-            id = msg['data'].split(' ')[0]
-            time.sleep(2)
-            self.redis.publish(f'worker-{ID}.result', f'{id} slept')
+            if 'shacrackprod' == msg['channel']:
+                STATE = 2
+                args = msg['data'].split(' ')
+                id = args.pop(0)
+                args[1] = int(args[1])
+                args[2] = int(args[2])
+                args[4] = int(args[4])
+                result = Sha256CrackProd(*args)
+                self.redis.publish(f'worker-{ID}.result', f'{id} {result}')
 
-        if 'shacrack' == msg['channel']:
-            args = msg['data'].split(' ')
-            id = args.pop(0)
-            result = Sha256Crack(args)
-            self.redis.publish(f'worker-{ID}.result', f'{id} {result}')
+            if exit_sign:
+                break
 
-        if 'shacrackprod' == msg['channel']:
-            args = msg['data'].split(' ')
-            id = args.pop(0)
-            args[1] = int(args[1])
-            args[2] = int(args[2])
-            args[4] = int(args[4])
-            result = Sha256CrackProd(*args)
-            self.redis.publish(f'worker-{ID}.result', f'{id} {result}')
+            STATE = 1
 
 
 if __name__ == '__main__':
     r = redis.Redis('redis', decode_responses=True)
-    worker = Listener(r)
-    while True:
-        worker.run()
-        time.sleep(0.001)
+    workerF = FunctionListener(r)
+    workerC = CmdListener(r)
+    workerF.start()
+    workerC.start()
+    STATE = 1
